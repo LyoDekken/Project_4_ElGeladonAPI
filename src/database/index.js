e82286bf4e86821a47d668dc22d72927e8d2d625
Humberto <humberto.araripe12@gmail.com>
2022-06-13 23:28:24 -0300

    SUGUNDO COMMIT

--- src/database/index.js
+++ src/database/index.js
@@ -3,13 +3,10 @@ import mongoose from 'mongoose';
 const { connect } = mongoose;
 
 export const conectarAoDatabase = () => {
-  connect(
-    'mongodb+srv://humberto_pan:12345@cluster0.gjlns.mongodb.net/elgeladon_db?retryWrites=true&w=majority',
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    },
-  )
+  connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
     .then(() => {
       console.log('MongoDB Conectado');
     })
